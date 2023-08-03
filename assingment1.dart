1975a19993169048a2eda8de437ece2425a3bd7c
Tareq Hossen <135951458+Tareq-Wasif@users.noreply.github.com>
2023-08-03 20:06:26 +0600

    Add files via upload

---
+++ assingment1.dart
@@ -0,0 +1,10 @@
+void main(){
+  const int a=12;
+  const int b=6;
+
+  print("Addition:12+6= ${a+b}");
+  print("Subtraction:12-6= ${a-b}");
+  print("Multiplicaton:12*6= ${a*b}");
+  print("Division:12/6=  ${a/b}");
+  print("Modulus:12%6= ${a%b}");
+}
